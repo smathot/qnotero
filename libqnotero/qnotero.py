ef62cf6bac1d2e252a443c88129f3b63ff90275b
Sebastiaan Mathot <s.mathot@cogsci.nl>
2011-11-02 12:12:00 +0100

    Bump to 0.47-pre1

--- libqnotero/qnotero.py
+++ libqnotero/qnotero.py
@@ -32,7 +32,7 @@ class Qnotero(QMainWindow):
 
 	"""The main class of the Qnotero GUI"""
 	
-	version = "0.46"
+	version = "0.47-pre1"
 
 	def __init__(self, systray=True, debug=False, reset=False, parent=None):
 
